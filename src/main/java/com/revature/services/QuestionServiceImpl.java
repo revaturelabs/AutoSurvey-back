1479053f8dc14380d0053a681e2ff9baa2d55db2
lestillero <lestillero@gmail.com>
2021-02-24 12:38:43 -0500

    added question service and controller

--- src/main/java/com/revature/services/QuestionServiceImpl.java
+++ src/main/java/com/revature/services/QuestionServiceImpl.java
@@ -1,5 +1,47 @@
 package com.revature.services;
 
+import java.sql.Timestamp;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.revature.beans.Question;
+import com.revature.repos.QuestionRepo;
+
+@Service
 public class QuestionServiceImpl implements QuestionService {
+
+	@Autowired
+	QuestionRepo qr;
+	
+	@Override
+	public Question getQuestion(int id) {
+		return qr.findById(id).get();
+	}
+
+	@Override
+	public List<Question> getAllQuestions() {
+		return qr.findAll();
+	}
+
+	@Override
+	public List<Question> getQuestionsByCreatedOn(Timestamp createdOn) {
+		return qr.findByCreatedOn(createdOn);
+	}
+
+	@Override
+	public Question addQuestion(Question q) {
+		return qr.save(q);
+	}
+
+	@Override
+	public Question updateQuestion(Question q) {
+		return qr.save(q);
+	}
+
+	@Override
+	public boolean deleteQuestion(int id) {
+		qr.delete(qr.findById(id).get());
+		return true;
+	}
 
 }
