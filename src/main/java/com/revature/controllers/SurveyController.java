86a2d96d006dd05ea0b12145f32f8fb9f85078d5
Eduardo Garcia <eduardo.garcia16@stjohns.edu>
2021-02-24 11:36:21 -0600

    Implemented Survey Services and Controllers

--- src/main/java/com/revature/controllers/SurveyController.java
+++ src/main/java/com/revature/controllers/SurveyController.java
@@ -1,5 +1,15 @@
 package com.revature.controllers;
 
+import java.util.List;
+
+import com.revature.beans.Survey;
+
 public interface SurveyController {
 
+	public Survey addSurvey(Survey survey);
+	public Survey getSurvey(int id);
+	public List<Survey> getAllSurveys();
+	public Survey updateSurvey(int id, Survey change);
+	public boolean deleteSurvey(int id);
+	
 }
