73218f8a3ad72d251612077179d749263605a1f9
Alexander Stachnik <alexander.stachnik@revature.net>
2021-02-24 12:20:22 -0500

    Added methods to answer bean

--- src/main/java/com/revature/beans/Answer.java
+++ src/main/java/com/revature/beans/Answer.java
@@ -30,4 +30,70 @@ public class Answer {
 	@Column(length=1024, name="content")
 	private String content;
 	
+	public Answer() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Answer [id=" + id + ", response=" + response + ", question=" + question + ", content=" + content + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Response getResponse() {
+		return response;
+	}
+
+	public void setResponse(Response response) {
+		this.response = response;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	// Equivalence is determined solely by the id field
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Answer other = (Answer) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+	
 }
