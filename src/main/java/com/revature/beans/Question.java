38bcd80e32282ebd56528fdaf4776957870c14ae
jeff5489 <jeff5489@gmail.com>
2021-02-24 12:18:08 -0500

    added question bean

--- src/main/java/com/revature/beans/Question.java
+++ src/main/java/com/revature/beans/Question.java
@@ -1,5 +1,71 @@
 package com.revature.beans;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name = "questions")
+@Table(name = "questions")
 public class Question {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="questions")
+	private int id;
+	@Column(name="content")
+	private String content;
+	@Column(name="createdOn")
+	private Timestamp createdOn;
+	
+	public Question() {
+		super();
+	}
+
+	public Question(String content, Timestamp createdOn) {
+		super();
+		this.content = content;
+		this.createdOn = createdOn;
+	}
+
+	public Question(int id, String content, Timestamp createdOn) {
+		super();
+		this.id = id;
+		this.content = content;
+		this.createdOn = createdOn;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Timestamp getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Timestamp createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	@Override
+	public String toString() {
+		return "Question [id=" + id + ", content=" + content + ", createdOn=" + createdOn + "]";
+	}
 
 }
