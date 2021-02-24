0ed934ce005e8df9cc0e64d2705cb209860746be
Alexander Stachnik <alexander.stachnik@revature.net>
2021-02-24 12:16:31 -0500

    Added answer bean

--- src/main/java/com/revature/beans/Answer.java
+++ src/main/java/com/revature/beans/Answer.java
@@ -1,5 +1,33 @@
 package com.revature.beans;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="answer")
 public class Answer {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id", updatable=false)
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name="response_id")
+	private Response response;
+	
+	@ManyToOne
+	@JoinColumn(name="question_id")
+	private Question question;
+	
+	//TODO: Adjust column length
+	@Column(length=1024, name="content")
+	private String content;
+	
 }
