f3f5a284aff9c303f3030bbaef4d4f232779a2b2
batur <batur@DESKTOP-M1BDIK3>
2021-02-24 12:54:05 -0500

    response

--- src/main/java/com/revature/controllers/ResponseController.java
+++ src/main/java/com/revature/controllers/ResponseController.java
@@ -1,5 +1,14 @@
 package com.revature.controllers;
 
+import java.util.List;
+
+import com.revature.beans.Response;
+
 public interface ResponseController {
 
+	boolean addResponse(Response r);
+	boolean deleteResponse(int id) ;
+	Response getResponse(int id) ;
+	List<Response> getAllResponse();
+	boolean updateResponse(int id,Response r);
 }
--- src/main/java/com/revature/services/ResponseServiceImpl.java
+++ src/main/java/com/revature/services/ResponseServiceImpl.java
@@ -1,5 +1,55 @@
 package com.revature.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.beans.Response;
+import com.revature.repos.ResponseRepo;
+
+@Service
 public class ResponseServiceImpl implements ResponseService {
 
+	@Autowired
+	ResponseRepo rr;
+
+	@Override
+	public boolean addResponse(Response r) {
+		// TODO Auto-generated method stub
+		return rr.save(r);
+	}
+
+	@Override
+	public Response getResponse(int id) {
+		// TODO Auto-generated method stub
+		return rr.findById(id).get();
+	}
+
+	@Override
+	public List<Response> getAllResponse() {
+		// TODO Auto-generated method stub
+		return (List<Response>) rr.findAll();
+	}
+
+	@Override
+	public boolean updateResponse(Response r) {
+		// TODO Auto-generated method stub
+		return rr.save(r);
+	}
+	
+	@Override
+	public boolean deleteResponse(int id) {
+		// TODO Auto-generated method stub
+		try {
+			rr.delete(rr.findById(id).get());
+			return true;
+		}catch(IllegalArgumentException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	
+	
 }
