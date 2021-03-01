2972fb9fedec9f6cd8378971dd6e739a0b01e0fc
camtheconqueror <weaver.cameron@gmail.com>
2021-03-01 16:16:54 -0600

    Update QuestionControllerTest

--- src/test/java/com/revature/controllers/QuestionControllerTest.java
+++ src/test/java/com/revature/controllers/QuestionControllerTest.java
@@ -1,6 +1,14 @@
 package com.revature.controllers;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.NoSuchElementException;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.sql.Timestamp;
 
 import org.junit.jupiter.api.Test;
@@ -9,8 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.google.gson.Gson;
 import com.revature.beans.Question;
 import com.revature.services.QuestionService;
 
@@ -24,6 +34,7 @@ public class QuestionControllerTest {
 	@Autowired
 	MockMvc mvc;
 
+	public static Gson gson = new Gson();
 	/**
 	 * (Write a succinct description of this method here.  If necessary,
 	 * additional paragraphs should be preceded by <p>, the html tag for
@@ -35,15 +46,12 @@ public class QuestionControllerTest {
 	 */
 	@Test
 	void getQuestion() throws Exception{
-		int id = 1;
-		Date date = new Date();
-		Timestamp ts = new Timestamp(date.getTime());
 		Question question = new Question();
-		question.setId(id);
-		question.setContent("Add Question Test");
-		question.setCreatedOn(ts);
-		
-		Mockito.when()
+		question.setId(1);
+		Mockito.when(serv.getQuestion(1)).thenReturn(question);
+		Mockito.when(serv.getQuestion(2)).thenThrow(NoSuchElementException.class);
+		mvc.perform(get("/question/1")).andExpect(status().isOk());
+		mvc.perform(get("/question/2")).andExpect(status().isOk());
 		
 	}
 
@@ -58,7 +66,13 @@ public class QuestionControllerTest {
 	 */
 	@Test
 	void getAllQuestions() throws Exception{
-
+		Question questionOne = new Question();
+		Question questionTwo = new Question();
+		ArrayList<Question> questions = new ArrayList<>();
+		questions.add(questionOne);
+		questions.add(questionTwo);
+		Mockito.when(serv.getAllQuestions()).thenReturn(questions);
+		mvc.perform(get("/question")).andExpect(status().isOk());
 	}
 
 	/**
@@ -72,7 +86,18 @@ public class QuestionControllerTest {
 	 */
 	@Test
 	void getQuestionsByCreatedOn() throws Exception{
-
+		Date date = new Date();
+		Question questionOne = new Question();
+		Question questionTwo = new Question();
+		ArrayList<Question> questions = new ArrayList<>();
+		questionOne.setId(1);
+		questionOne.setCreatedOn(new Timestamp(date.getTime()));
+		questionTwo.setId(2);
+		questionTwo.setCreatedOn(new Timestamp(date.getTime()));
+		questions.add(questionOne);
+		questions.add(questionTwo);
+		Mockito.when(serv.getQuestionsByCreatedOn(new Timestamp(date.getTime()))).thenReturn(questions);
+		mvc.perform(get("/questionByCreatedOn/" + new Timestamp(date.getTime()))).andExpect(status().isOk());
 	}
 
 
@@ -87,6 +112,14 @@ public class QuestionControllerTest {
 	 */
 	@Test	
 	void updateQuestion() throws Exception{
+		Question questionOne = new Question();
+		questionOne.setId(1);
+		questionOne.setContent("Test content 1");
+		Question questionTwo = new Question();
+		questionTwo.setId(2);
+		questionTwo.setContent("Test content 2");
+		Mockito.when(serv.updateQuestion(questionTwo)).thenReturn(questionTwo);
+		mvc.perform(put("/question/1").contentType(MediaType.APPLICATION_JSON).content(gson.toJson(questionOne))).andExpect(status().isOk());
 
 	}
 
@@ -101,6 +134,9 @@ public class QuestionControllerTest {
 	 */
 	@Test
 	void deleteQuestion() throws Exception{
-
+		Question question = new Question();
+		question.setId(1);
+		Mockito.when(serv.deleteQuestion(1)).thenReturn(true);
+		mvc.perform(delete("/question/1")).andExpect(status().isOk());
 	}
 }
