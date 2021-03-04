945363d7ec47a251bcb97294be20ec0c79d8347d
root <root@ip-172-31-42-140.ec2.internal>
2021-03-04 22:48:43 +0000

    Got .war working on Tomcat

--- src/main/java/com/example/demo/QcDataReportAppApplication.java
+++ src/main/java/com/example/demo/QcDataReportAppApplication.java
@@ -5,15 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ComponentScan("com.revature")
 @EnableJpaRepositories("com.revature.repos")
 @EntityScan("com.revature.beans")
-public class QcDataReportAppApplication {
+public class QcDataReportAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QcDataReportAppApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(QcDataReportAppApplication.class);
+	}
+
 }
+
