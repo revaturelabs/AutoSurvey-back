765a3dc247602e7df9ca28c23bec95872ae38974
emilytroll <76971112+emilytroll@users.noreply.github.com>
2021-03-09 12:41:57 -0500

    pushing upload :(

--- src/test/java/com/revature/steps/UploadPageSteps.java
+++ src/test/java/com/revature/steps/UploadPageSteps.java
@@ -18,9 +18,37 @@ public class UploadPageSteps {
 	String url = "http://ec2-54-173-212-237.compute-1.amazonaws.com:8080/AutoSurvey/upload.html";
 	String testfile = "C:\\\\Users\\\\Ric\\\\Documents\\\\uploadtest.txt";
 	
+	@Given("^The user is logged in as admin$")
+	public void the_User_is_logged_in_as_admin()
+	{
+			driver.get("http://ec2-54-173-212-237.compute-1.amazonaws.com:8080/AutoSurvey/");
+			
+				uploadpage.email.sendKeys("admin@admin.com"); // to be changed to test user when that gets created
+			    try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			    
+			    uploadpage.password.sendKeys("admin");
+			    try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			    
+			    uploadpage.loginButton.click();
+			    try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+	}
+
+	
 	@Given("^The user is on the upload page$")
 	public void the_user_is_on_the_upload_page() {
-	    driver.get(url);
+		driver.get(url);
 	    try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
@@ -30,6 +58,7 @@ public class UploadPageSteps {
 
 	@When("^The user clicks the choose file button$")
 	public void the_user_clicks_the_choose_file_button() {
+		uploadpage.uploadButton.click();
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
@@ -45,11 +74,6 @@ public class UploadPageSteps {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-	}
-
-	@Then("^The file name should appear$")
-	public void the_file_name_should_appear() {
-	    Assertions.assertNotEquals("uploadtest.txt", uploadpage.uploadCsv);
 	}
 
 	@When("^The user clicks the upload button$")
@@ -79,4 +103,5 @@ public class UploadPageSteps {
 		}
 	}
 	
+
 }
--- src/test/resources/Upload.feature
+++ src/test/resources/Upload.feature
@@ -1,13 +1,11 @@
 Feature: Tests if the user can upload a file
 	
+Background:
+	Given The user is logged in as admin
+
 Scenario: User clicks the choose file button
 	Given The user is on the upload page
 	When The user clicks the choose file button
 	And The user selects a file
-	Then The file name should appear
-	
-Scenario: User clicks the upload button
-	Given The user is on the upload page
-	When The user clicks the upload button
+	And The user clicks the upload button
 	Then The file should be uploaded
-	
