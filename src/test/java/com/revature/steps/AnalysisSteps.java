13e0cad04d177578ab836164052888424cf828d9
Ric <ricdorellana@gmail.com>
2021-03-09 11:24:11 -0600

    Fixed Analysis Page

--- src/test/java/com/revature/steps/AnalysisSteps.java
+++ src/test/java/com/revature/steps/AnalysisSteps.java
@@ -12,15 +12,47 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class AnalysisSteps {
-	
+
 	public static WebDriver driver = AnalysisRunner.driver;
 	public static AnalysisPage analysispages = AnalysisRunner.analysispage;
 	String url = "http://ec2-54-173-212-237.compute-1.amazonaws.com:8080/AutoSurvey/analysis.html";
-	
+
+	@Given("^the user is logged in$")
+	public void the_user_is_logged_in() throws Throwable {
+		driver.get("http://ec2-54-173-212-237.compute-1.amazonaws.com:8080/AutoSurvey/");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		analysispages.email.sendKeys("admin@admin.com");
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		analysispages.password.sendKeys("admin");
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		analysispages.loginButton.click();
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+
+	}
+
 	@Given("^the user is on the Analysis Page$")
 	public void the_user_is_on_the_Analysis_Page() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	  driver.get(url);
+		// Write code here that turns the phrase above into concrete actions
+		Assertions.assertEquals("Revature | QC Analysis", driver.getTitle());
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e1) {
@@ -30,10 +62,10 @@ public class AnalysisSteps {
 
 	@When("^the user clicks a start date on the Calendar$")
 	public void the_user_clicks_a_start_date_on_the_Calendar() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    analysispages.date.click();
+		// Write code here that turns the phrase above into concrete actions
+		analysispages.date.click();
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(2000);
 		} catch (InterruptedException e1) {
 			e1.printStackTrace();
 		}
@@ -41,8 +73,8 @@ public class AnalysisSteps {
 
 	@When("^the user clicks on the Search button$")
 	public void the_user_clicks_on_the_Search_button() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    analysispages.searchBtn.click();
+		// Write code here that turns the phrase above into concrete actions
+		analysispages.searchBtn.click();
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e1) {
@@ -52,79 +84,84 @@ public class AnalysisSteps {
 
 	@Then("^the selected week analysis displays$")
 	public void the_selected_week_analysis_displays() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-		  Assertions.assertEquals(true, true);
-	   
+		// Write code here that turns the phrase above into concrete actions
+		Assertions.assertEquals(true, true);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+
 	}
 
 	@Given("^the user is on the default Analysis Page$")
 	public void the_user_is_on_the_default_Analysis_Page() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-		
+		// Write code here that turns the phrase above into concrete actions
+
 		Assertions.assertEquals(url, driver.getCurrentUrl());
-		
+
 	}
 
 	@When("^the User Clicks on the Batch Tab$")
 	public void the_User_Clicks_on_the_Batch_Tab() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    analysispages.batchTab.click();
-	    try {
+		// Write code here that turns the phrase above into concrete actions
+		analysispages.batchTab.click();
+		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e1) {
 			e1.printStackTrace();
 		}
-	    
+
 	}
 
 	@When("^the user Selects a Location$")
 	public void the_user_Selects_a_Location() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
+		// Write code here that turns the phrase above into concrete actions
 		Select location = new Select(analysispages.location);
 		location.selectByIndex(0);
-		
-		  try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
 	}
 
 	@When("^the user Selects a Batch$")
 	public void the_user_Selects_a_Batch() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
+		// Write code here that turns the phrase above into concrete actions
 		Select batch = new Select(analysispages.batch);
-		
+
 		batch.selectByIndex(0);
-		  try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
 	}
 
 	@When("^the user Selects a Week$")
 	public void the_user_Selects_a_Week() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
+		// Write code here that turns the phrase above into concrete actions
 		Select week = new Select(analysispages.week);
 		week.selectByIndex(0);
-		
+
 	}
 
 	@When("^the user clicks on the Search Button$")
 	public void the_user_clicks_on_the_Search_Button() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-		   analysispages.searchBtn.click();
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
+		// Write code here that turns the phrase above into concrete actions
+		analysispages.searchBtn.click();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
 	}
 
 	@Then("^the selected Batch Analysis displays$")
 	public void the_selected_Batch_Analysis_displays() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	  Assertions.assertEquals(true, true);
+		// Write code here that turns the phrase above into concrete actions
+		Assertions.assertEquals(true, true);
 	}
 }
